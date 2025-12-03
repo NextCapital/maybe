41b51934750c03bc2c06f89ec3d291276956d181
Mike Kreiser <kreiserm@nextcapital.com>
2025-12-03 11:28:20 -0600

    Add TypeScript types to AsyncQueue.ts [ENG-6706]

--- js/async-queue/AsyncQueue.ts
+++ js/async-queue/AsyncQueue.ts
@@ -1,4 +1,8 @@
-const PromiseUtils = require('../promise-utils/PromiseUtils');
+import PromiseUtils, { Deferred } from '../promise-utils/PromiseUtils.js';
+
+interface AsyncQueueOptions {
+  maxConcurrency?: number;
+}
 
 /**
  * A queue for performing async tasks with a set amount of tasks allowed to run at once.
@@ -6,41 +10,34 @@ const PromiseUtils = require('../promise-utils/PromiseUtils');
  * If there is room in the queue, a new task will start running immediately. Otherwise, it
  * will be added to the queue. When a running task completes or fails, the next task in the queue
  * will start running.
- *
- * @class
  */
-class AsyncQueue {
+export default class AsyncQueue {
+  maxConcurrency: number;
+  private queue: Array<() => void>;
+  private numRunningTasks: number;
+
   /**
    * A queue for performing async tasks with a maximum concurrency.
-   *
-   * @param {object} [options={}] All constructor options.
-   * @param {number} [options.maxConcurrency=1] Max number of async tasks that can run at once.
    */
-  constructor({ maxConcurrency = 1 } = {}) {
+  constructor({ maxConcurrency = 1 }: AsyncQueueOptions = {}) {
     this.maxConcurrency = maxConcurrency;
     this.queue = [];
-
     this.numRunningTasks = 0;
   }
 
   /**
    * The length of the queue instance.
-   *
-   * @type {number}
    */
-  get length() {
+  get length(): number {
     return this.queue.length;
   }
 
   /**
    * Performs the task immediately if possible, otherwise adds it to the queue to be performed
    * when a running task completes.
-   *
-   * @param {Function} task Function returning a promise. Should not do anything until invoked.
-   * @returns {Promise} Promise that resolves or rejects with the task result.
    */
-  perform(task) {
-    const result = PromiseUtils.defer();
+  perform<TResult>(task: () => Promise<TResult>): Promise<TResult> {
+    const result = PromiseUtils.defer<TResult>();
 
     if (this.numRunningTasks < this.maxConcurrency) {
       // task can be performed immediately
@@ -57,27 +54,20 @@ class AsyncQueue {
 
   /**
    * Actually runs a given task.
-   *
-   * @param {Deferred} result Output from `PromiseUtils.defer`.
-   * @param {Function} task The task to perform.
-   * @returns {Promise}
-   * @private
    */
-  _performTask(result, task) {
+  private _performTask<TResult>(result: Deferred<TResult>, task: () => Promise<TResult>): Promise<void> {
     return Promise.resolve(task()).then((value) => {
       result.resolve(value);
     }).catch((ex) => {
-      result.reject(ex);
+      result.reject(ex as Error);
     }).finally(() => {
       // now that we are complete, a task from the queue can run
       if (this.queue.length) {
         const newTask = this.queue.shift();
-        newTask();
+        newTask!();
       } else {
         this.numRunningTasks -= 1;
       }
     });
   }
 }
-
-module.exports = AsyncQueue;
