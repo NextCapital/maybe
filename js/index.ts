65a469852c416b840e77c74d51bf3a5676ae0c4f
Mike Kreiser <kreiserm@nextcapital.com>
2025-12-03 11:31:17 -0600

    Update index.ts with ES module exports [ENG-6706]

--- js/index.ts
+++ js/index.ts
@@ -1,11 +1,4 @@
-const AsyncQueue = require('./async-queue/AsyncQueue');
-const Maybe = require('./maybe/Maybe');
-const PromiseUtils = require('./promise-utils/PromiseUtils');
-const PendingValueError = require('./maybe/PendingValueError');
-
-module.exports = {
-  AsyncQueue,
-  Maybe,
-  PromiseUtils,
-  PendingValueError
-};
+export { default as AsyncQueue } from './async-queue/AsyncQueue.js';
+export { default as Maybe } from './maybe/Maybe.js';
+export { default as PromiseUtils, Deferred } from './promise-utils/PromiseUtils.js';
+export { default as PendingValueError } from './maybe/PendingValueError.js';
